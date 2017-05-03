e760171cde3b1a9e59598d43125c93b2941d1600
Dylan Wallace <dxdylan777@gmail.com>
2017-05-02 21:41:01 -0700

    Added comments

--- UDP/Listener.c
+++ UDP/Listener.c
@@ -4,94 +4,94 @@ Author: Dylan Wallace, Drones and Autonomous Systems Lab, University of Nevada, 
 A simple C UDP listener to listen to the port defined. Receives the message sent to the defined port.
 Made with the help of Beej's Guide to Network Programming.*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include <stdio.h>      // Standard Input/Output library
+#include <stdlib.h>     // Standard library
+#include <unistd.h>     // Unix API
+#include <errno.h>      // Error library
+#include <string.h>     // String library
+#include <sys/types.h>  // System types library
+#include <sys/socket.h> // Sockets library
+#include <netinet/in.h> // Main internet library
+#include <arpa/inet.h>  // The Internet library
+#include <netdb.h>      // Internet database
 
-#define MYPORT "4812"    // the port users will be connecting to
+#define MYPORT "4812"   // The port users will be connecting to
 
-#define MAXBUFLEN 100
+#define MAXBUFLEN 100   // The maximum receiving buffer
 
-// get sockaddr, IPv4 or IPv6:
+// Function to get sockaddr, IPv4 or IPv6
 void *get_in_addr(struct sockaddr *sa)
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
+    if (sa->sa_family == AF_INET) { // If IPv4
+        return &(((struct sockaddr_in*)sa)->sin_addr); // Return IPv4
     }
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr); // Return IPv6
 }
 
 int main(void)
 {
-    int sockfd;
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    int numbytes;
-    struct sockaddr_storage their_addr;
-    char buf[MAXBUFLEN];
-    socklen_t addr_len;
-    char s[INET6_ADDRSTRLEN];
+    int sockfd; // Socket descriptor
+    struct addrinfo hints, *servinfo, *p; // Used to store IP address info
+    int rv; // Used for error checking
+    int numbytes; // Number of bytes received
+    struct sockaddr_storage their_addr; // Store the talker IP address
+    char buf[MAXBUFLEN]; // Receiving buffer
+    socklen_t addr_len; // IP address length
+    char s[INET6_ADDRSTRLEN]; // String to store IP address
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+    memset(&hints, 0, sizeof hints); // Fill structure with empty info
+    hints.ai_family = AF_UNSPEC; // Don't care if IPv4 or IPv6 (set to AF_INET to force IPv4)
+    hints.ai_socktype = SOCK_DGRAM; // UDP type
+    hints.ai_flags = AI_PASSIVE; // Use my IP to listen to the socket
 
-    if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) { // If getaddrinfo fails
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv)); // Give getaddrinfo error
         return 1;
     }
 
-    // loop through all the results and bind to the first we can
+    // Loop through all the results and bind to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("listener: socket");
+                p->ai_protocol)) == -1) { // If socket creation error
+            perror("listener: socket"); // Give socket error
             continue;
         }
 
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("listener: bind");
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) { // If bind fails
+            close(sockfd); // Close the socket
+            perror("listener: bind"); // Give bind error
             continue;
         }
 
         break;
     }
 
-    if (p == NULL) {
-        fprintf(stderr, "listener: failed to bind socket\n");
+    if (p == NULL) { // If nothing ever bound to the socket
+        fprintf(stderr, "listener: failed to bind socket\n"); // Give failure to bind message
         return 2;
     }
 
-    freeaddrinfo(servinfo);
+    freeaddrinfo(servinfo); // Free the structure
 
-    printf("listener: waiting to recvfrom...\n");
+    printf("listener: waiting to recvfrom...\n"); // Give waiting to receive message
 
     addr_len = sizeof their_addr;
     if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
-        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-        perror("recvfrom");
+        (struct sockaddr *)&their_addr, &addr_len)) == -1) { // If receiving error
+        perror("recvfrom"); // Give receive error
         exit(1);
     }
 
     printf("listener: got packet from %s\n",
         inet_ntop(their_addr.ss_family,
             get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s));
-    printf("listener: packet is %d bytes long\n", numbytes);
-    buf[numbytes] = '\0';
-    printf("listener: packet contains \"%s\"\n", buf);
+            s, sizeof s)); // Print received message and converted IP address
+    printf("listener: packet is %d bytes long\n", numbytes); // Give number of bytes in the packet
+    buf[numbytes] = '\0'; // Add null-terminator
+    printf("listener: packet contains \"%s\"\n", buf); // Print packet contents
 
-    close(sockfd);
+    close(sockfd); // Close the socket
 
     return 0;
 }
--- UDP/Talker.c
+++ UDP/Talker.c
@@ -4,66 +4,66 @@ Author: Dylan Wallace, Drones and Autonomous Systems Lab, University of Nevada, 
 A simple C UDP talker to broadcast a message to the port defined. Allows the user to input the IP address, and connects to the defined port.
 The user can input the desired message to send. Made with the help of Beej's Guide to Network Programming.*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include <stdio.h>      // Standard Input/Output library
+#include <stdlib.h>     // Standard library
+#include <unistd.h>     // Unix API
+#include <errno.h>      // Error library
+#include <string.h>     // String library
+#include <sys/types.h>  // System types library
+#include <sys/socket.h> // Sockets library
+#include <netinet/in.h> // Main internet library
+#include <arpa/inet.h>  // The Internet library
+#include <netdb.h>      // Internet database
 
-#define SERVERPORT "4812"    // the port users will be connecting to
+#define SERVERPORT "4812" // The port users will be connecting to
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) // Uses command line arguments
 {
-    int sockfd;
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    int numbytes;
+    int sockfd; // Socket descriptor
+    struct addrinfo hints, *servinfo, *p; // Structs to store IP address info
+    int rv; // Error checking
+    int numbytes; // Number of bytes to send
 
-    if (argc != 3) {
-        fprintf(stderr,"usage: talker hostname message\n");
+    if (argc != 3) { // Check command line arguments
+        fprintf(stderr,"usage: talker hostname message\n"); // Print usage if needed
         exit(1);
     }
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
+    memset(&hints, 0, sizeof hints); // Fill out empty structure
+    hints.ai_family = AF_UNSPEC; // We don't care if IPv4 or IPv6 (set to AF_INET to force IPv4)
+    hints.ai_socktype = SOCK_DGRAM; // UDP type
 
-    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) { // If getaddrinfo fails
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv)); // Give getaddrinfo error
         return 1;
     }
 
-    // loop through all the results and make a socket
+    // Loop through all the results and make a socket
     for(p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("talker: socket");
+                p->ai_protocol)) == -1) { // If socket create fails
+            perror("talker: socket"); // Give socket error
             continue;
         }
 
         break;
     }
 
-    if (p == NULL) {
-        fprintf(stderr, "talker: failed to create socket\n");
+    if (p == NULL) { // Check if anything actually ever was created
+        fprintf(stderr, "talker: failed to create socket\n"); // Give failure to create message
         return 2;
     }
 
     if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
-             p->ai_addr, p->ai_addrlen)) == -1) {
-        perror("talker: sendto");
+             p->ai_addr, p->ai_addrlen)) == -1) { // If sending error
+        perror("talker: sendto"); // Give sending error
         exit(1);
     }
 
-    freeaddrinfo(servinfo);
+    freeaddrinfo(servinfo); // Free the IP address structure
 
-    printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
-    close(sockfd);
+    printf("talker: sent %d bytes to %s\n", numbytes, argv[1]); // Print number of bytes sent to the IP address
+    close(sockfd); // Close the socket
 
     return 0;
 }
